eaea59903c60480319680ad9c9ccf2bdb23ded98
hydrazin60 <sureshkhdk45@gmail.com>
2024-07-31 12:27:48 +0545

    star background components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import { ThreeDCardDemo } from "./components/Card/ThreeDCardDemo";
 import { CompareDemo } from "./components/Card/CompareDemo";
 import { GoogleGeminiEffectDemo } from "./components/Card/GoogleGeminiEffectDemo";
 import { SparklesPreview } from "./components/Card/SparklesPreview";
+import { MeteorsDemo } from "./components/Card/MeterorsDemo";
+import { ShootingStarsAndStarsBackgroundDemo } from "./components/Card/ShootingStarsAndStarsBackgroundDemo";
 
 export default function App() {
   return (
@@ -11,8 +13,9 @@ export default function App() {
         <ThreeDCardDemo />
         <CompareDemo />
         <GoogleGeminiEffectDemo />
-        {/* <GoogleGeminiEffectDemo /> */}
-        <SparklesPreview/>
+        <SparklesPreview />
+        <MeteorsDemo />
+        <ShootingStarsAndStarsBackgroundDemo />F
       </div>
     </div>
   );
