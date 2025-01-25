55ab04e5e4b836459b64da57bf41cef2dbe39f2d
hydrazin60 <sureshkhdk45@gmail.com>
2025-01-25 14:25:02 +0545

    jhhh

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,17 +4,40 @@
 //   theme: {
 //     extend: {},
 //   },
+//   theme: {
+//     // your existing config
+//     extend: {
+//       animation: {
+//         "meteor-effect": "meteor 5s linear infinite",
+//       },
+//       keyframes: {
+//         meteor: {
+//           "0%": { transform: "rotate(215deg) translateX(0)", opacity: "1" },
+//           "70%": { opacity: "1" },
+//           "100%": {
+//             transform: "rotate(215deg) translateX(-500px)",
+//             opacity: "0",
+//           },
+//         },
+//       },
+//     },
+//   },
 //   plugins: [],
 // };
 
+
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  // darkMode: "class",
   theme: {
-    extend: {},
-  },
-  theme: {
-    // your existing config
     extend: {
       animation: {
         "meteor-effect": "meteor 5s linear infinite",
@@ -31,5 +54,17 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [addVariablesForColors],
 };
+
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
